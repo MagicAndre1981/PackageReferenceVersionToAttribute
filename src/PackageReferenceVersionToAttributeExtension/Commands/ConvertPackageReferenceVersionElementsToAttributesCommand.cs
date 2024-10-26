8306d3bf5c20360d5f4c597daf086ca3b5b4d2d6
Rami <icnocop@users.noreply.github.com>
2024-10-25 17:52:50 -0700

    Created initial VSIX and demo library

---
+++ src/PackageReferenceVersionToAttributeExtension/Commands/ConvertPackageReferenceVersionElementsToAttributesCommand.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="ConvertPackageReferenceVersionElementsToAttributesCommand.cs" company="Rami Abughazaleh">
+//   Copyright (c) Rami Abughazaleh. All rights reserved.
+// </copyright>
+
+namespace PackageReferenceVersionToAttributeExtension
+{
+    using System.Threading.Tasks;
+    using Community.VisualStudio.Toolkit;
+    using EnvDTE;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// Convert PackageReference Version elements to attributes command.
+    /// </summary>
+    [Command(
+        PackageGuids.guidPackageReferenceVersionToAttributeExtensionCmdSetString,
+        PackageIds.PackageReferenceVersionToAttributeCommand)]
+    internal sealed class ConvertPackageReferenceVersionElementsToAttributesCommand : BaseCommand<ConvertPackageReferenceVersionElementsToAttributesCommand>
+    {
+        /// <inheritdoc/>
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("PackageReferenceVersionToAttributeExtension", "Button clicked");
+        }
+    }
+}
---
+++ src/PackageReferenceVersionToAttributeExtension/PackageReferenceVersionToAttributeExtensionPackage.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="PackageReferenceVersionToAttributeExtensionPackage.cs" company="Rami Abughazaleh">
+//   Copyright (c) Rami Abughazaleh. All rights reserved.
+// </copyright>
+
+namespace PackageReferenceVersionToAttributeExtension
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+    using Community.VisualStudio.Toolkit;
+    using Microsoft.VisualStudio.Shell;
+    using Task = System.Threading.Tasks.Task;
+
+    /// <summary>
+    /// PackageReference Version to attribute extension package.
+    /// </summary>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidPackageReferenceVersionToAttributeExtensionString)]
+    [ProvideUIContextRule(
+        contextGuid: PackageGuids.guidPackageReferenceVersionToAttributeExtensionUIRuleString,
+        name: "Csproj",
+        expression: "Csproj",
+        termNames: ["Csproj"],
+        termValues: ["ActiveProjectCapability:CSharp"])]
+    public sealed class PackageReferenceVersionToAttributeExtensionPackage : ToolkitPackage
+    {
+        /// <inheritdoc/>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
